837f154c5bc94346749464eccab6e31313aea7c4
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-12 21:49:04 +0300

    add add orm and inline orm for company or my site

--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Company, Phone, SocialMediaLinks
 
-# Register your models here.
+# Inline model for Phone related to Company
+class PhoneInline(admin.TabularInline):
+    model = Phone
+    readonly_fields = ('id',)
+    extra = 1
+
+# Inline model for SocialMediaLinks related to Company
+class SocialMediaLinksInline(admin.TabularInline):
+    model = SocialMediaLinks
+    readonly_fields = ('id',)
+    extra = 1
+
+# Admin for Company, including inlines for Phone and SocialMediaLinks
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    inlines = [PhoneInline, SocialMediaLinksInline]
+
+# Registering Phone and SocialMediaLinks separately
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'company')
+
+@admin.register(SocialMediaLinks)
+class SocialMediaLinksAdmin(admin.ModelAdmin):
+    list_display = ('account_name', 'account_link', 'company')
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
+
+class Company(models.Model):
+    title = models.CharField(max_length=20)
+    logo = models.ImageField(upload_to='logo')
+    about = models.TextField(max_length=300)
+    email = models.EmailField()
+    location = models.CharField(max_length=20)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Phone(models.Model):
+    phone = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.phone
+
+
+class SocialMediaLinks(models.Model):
+    account_name = models.CharField(max_length=20)
+    account_link = models.CharField(max_length=200)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.account_name
