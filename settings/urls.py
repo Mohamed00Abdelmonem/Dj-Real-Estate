e86403829c3fea2c2414636423b9d8130b71fa4a
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-13 18:12:15 +0300

    add templates and logic for about page and contact page

--- settings/urls.py
+++ settings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, about , contact
 
 
 urlpatterns = [
-    path('', home, name='settings'),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
 ]
--- settings/views.py
+++ settings/views.py
@@ -3,5 +3,15 @@ from .models import Company, Phone, SocialMediaLinks
 # Create your views here.
 
 def home(request):
-    
     return render(request, 'settings/index.html')
+
+def about(request):
+    data = Company.objects.last()
+    return render(request, 'settings/about.html', {'data':data})
+
+def contact(request):
+    data = Company.objects.last()
+    phone = Phone.objects.all()
+    social_media = SocialMediaLinks.objects.all()
+    return render(request, 'settings/contact.html', {'data':data})    
+    
