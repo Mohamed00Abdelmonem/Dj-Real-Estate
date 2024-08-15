1d88b67efe6b673b37f9229bbdfb6957368b8da8
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-15 10:00:40 +0300

    add single property page

--- property/urls.py
+++ property/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PropertyList
+from .views import PropertyList, PropertyDetail
 
 app_name = 'property'
 
 urlpatterns = [
     path('', PropertyList.as_view(), name='property_list'),
+    path('/<int:pk>', PropertyDetail.as_view(), name='property_detial'),
 ]
--- property/views.py
+++ property/views.py
@@ -11,3 +11,8 @@ class PropertyList(ListView):
     model = Property
     template_name = 'property_list.html'
     context_object_name = 'properties'
+
+class PropertyDetail(DetailView):
+    model = Property
+    template_name = 'property_detail.html'
+    context_object_name = 'property'
