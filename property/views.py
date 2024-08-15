17a045dc94f1a37099c63e3328ee5ae442d0e65e
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-15 19:23:32 +0300

    add images_property many in property detiail page

--- property/views.py
+++ property/views.py
@@ -21,6 +21,7 @@ class PropertyDetail(DetailView):
     template_name = 'property_detail.html'
     context_object_name = 'property'
 
+    
     def get_object(self):
         obj = super().get_object()
         obj.view_count += 1
